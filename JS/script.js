18f9f27129ab9ab661d68b470b1f89712ab937d5
Kelvin <kelisakami@gmail.com>
2020-11-07 11:55:40 -0500

    Add scroll btn

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+const btnScroll = document.querySelector('.btn-scroll');
+
+window.addEventListener('scroll', scrollFunction);
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnScroll.style.display = "block";
+  } else {
+    btnScroll.style.display = "none";
+  }
+}
+
+
+btnScroll.addEventListener('click',() =>{
+    document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} )
